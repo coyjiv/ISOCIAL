9fb32f2bde06c34cd4f8fc5d14b8bca67f308f50
akiselevych <anton.kiselevych@icapgroupgmbh.com>
2024-01-20 20:31:51 +0200

    added cors

--- backend/src/main/java/com/coyjiv/isocial/configs/SecurityConfig.java
+++ backend/src/main/java/com/coyjiv/isocial/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.coyjiv.isocial.configs;
 import com.coyjiv.isocial.auth.DefaultAuthenticationSuccessHandler;
 import com.coyjiv.isocial.filters.JwtValidatorFilter;
 import com.coyjiv.isocial.service.auth.OAuthUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.util.Collections;
@@ -43,6 +47,18 @@ public class SecurityConfig {
     return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
             .csrf(AbstractHttpConfigurer::disable)
+            // TODO: REMOVE IN PRODUCTION
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+              @Override
+              public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("*"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L);
+                return config;
+              }
+            }))
             .httpBasic(AbstractHttpConfigurer::disable)
             .addFilterBefore(new JwtValidatorFilter(), UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(req ->
