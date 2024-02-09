1a980af7d1a8b35a8843da2dd508cdf534a348ea
akiselevych <anton.kiselevych@icapgroupgmbh.com>
2024-02-09 16:20:59 +0200

    working on post component

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,9 +1,18 @@
 import { withLayout } from "../../hooks/withLayout"
 import {Link} from "react-router-dom";
+import Post from "../../components/Post/Post.jsx";
 
 const HomePage = () => {
   return (
-    <Link to="/login">Log In</Link>
+    // <Link to="/login">Log In</Link>
+      <Post postId={1} authorId={1}
+          avatarUrl={'https://cdn-icons-png.flaticon.com/512/5556/5556512.png'}
+          username={'Anastasia Uskova'}
+          creationDate={new Date()}
+          textContent={'Oohhh, sci-fi satisfaction'}
+          images={[
+              'https://s.ill.in.ua/i/news/570x380/291/291360.jpg'
+          ]}/>
   )
 }
 
