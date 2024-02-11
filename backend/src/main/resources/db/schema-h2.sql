9f14e9e19fdc645a27ecc68abb0c2b576da82e42
Aleksey Chernov <chernov.aleksey.ukraine@gmail.com>
2024-02-11 21:21:45 +0200

    deactivation for favorites is  fixed 110224

--- backend/src/main/resources/db/schema-h2.sql
+++ backend/src/main/resources/db/schema-h2.sql
@@ -79,3 +79,26 @@ CREATE TABLE friends
     last_modified_date TIMESTAMP,
     is_active BOOLEAN NOT NULL DEFAULT FALSE
 );
+DROP TABLE IF EXISTS posts;
+CREATE TABLE public.posts
+(
+    id                 INT AUTO_INCREMENT PRIMARY KEY,
+    text_content       VARCHAR(1000),
+    attachments        VARCHAR ARRAY,
+    is_edited          BOOLEAN NOT NULL DEFAULT FALSE,
+    original_post_id   INT,
+    user_id            INT REFERENCES users (id),
+    creation_date      TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    is_active BOOLEAN  NOT NULL DEFAULT FALSE
+);
+DROP TABLE IF EXISTS favorites;
+CREATE TABLE public.favorites
+(
+    id                 INT AUTO_INCREMENT PRIMARY KEY,
+    selected_post_id   INT,
+    user_selector_id   INT REFERENCES users (id),
+    creation_date      TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    is_active BOOLEAN  NOT NULL DEFAULT FALSE
+);
