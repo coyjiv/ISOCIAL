8e1fdac5824b5bdea470fafad237d2df8657c9e5
akiselevych <anton.kiselevych@icapgroupgmbh.com>
2024-02-13 10:03:55 +0200

    validation

--- backend/src/main/java/com/coyjiv/isocial/domain/User.java
+++ backend/src/main/java/com/coyjiv/isocial/domain/User.java
@@ -1,6 +1,14 @@
 package com.coyjiv.isocial.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
