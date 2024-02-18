48497d624d59356b382f995cc352c0117da5a3d5
Daniil Shcherbakov <djdaniil46@gmail.com>
2024-02-19 00:43:31 +0100

    fixes

--- backend/src/main/java/com/coyjiv/isocial/transfer/user/UserUpdateRequestMapper.java
+++ backend/src/main/java/com/coyjiv/isocial/transfer/user/UserUpdateRequestMapper.java
@@ -28,9 +28,6 @@ public class UserUpdateRequestMapper extends DtoMapperFacade<User, UserUpdateReq
     entity.setBannerUrl(dto.getBannerUrl());
     entity.setDateOfBirth(dto.getDateOfBirth());
     entity.setGender(dto.getGender());
-    entity.setPremium(dto.isPremium());
-    entity.setPremiumNickname(dto.isPremium() ? dto.getPremiumNickname() : null);
-    entity.setPremiumEmoji(dto.isPremium() ? dto.getPremiumEmoji() : null);
 
   }
 }
