b27e431a9f940bbefcf4c0f3fa753e98a33dd317
Daniil Shcherbakov <djdaniil46@gmail.com>
2024-02-20 02:32:19 +0100

    urgent prod fix

--- backend/src/main/resources/db/schema-postgres.sql
+++ backend/src/main/resources/db/schema-postgres.sql
@@ -10,21 +10,25 @@ CREATE TABLE public.users
     avatars_url        TEXT[],
     banner_url         VARCHAR,
     bio                VARCHAR,
-    is_private         BOOLEAN NOT NULL DEFAULT FALSE,
+    is_private         BOOLEAN      NOT NULL DEFAULT FALSE,
     last_seen          TIMESTAMP,
-    activity_status    VARCHAR(10) NOT NULL DEFAULT 'OFFLINE',
-    gender             VARCHAR(20) NOT NULL,
+    activity_status    VARCHAR(10)  NOT NULL DEFAULT 'OFFLINE',
+    gender             VARCHAR(20)  NOT NULL,
     date_of_birth      DATE,
     creation_date      TIMESTAMP,
     last_modified_date TIMESTAMP,
-    is_active          BOOLEAN NOT NULL DEFAULT FALSE
+    is_premium         BOOLEAN      NOT NULL DEFAULT FALSE,
+    premium_nickname   VARCHAR(250),
+    premium_emoji      VARCHAR(250),
+    is_active          BOOLEAN      NOT NULL DEFAULT FALSE
 );
+
 
 DROP TABLE IF EXISTS public.roles CASCADE;
 CREATE TABLE public.roles
 (
     id      SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
+    user_id INT         NOT NULL,
     name    VARCHAR(50) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES public.users (id)
 );
@@ -53,7 +57,7 @@ CREATE TABLE public.messages
     is_edited          BOOLEAN,
     creation_date      TIMESTAMP,
     last_modified_date TIMESTAMP,
-    is_active          BOOLEAN NOT NULL DEFAULT FALSE,
+    is_active          BOOLEAN     NOT NULL DEFAULT FALSE,
     FOREIGN KEY (chat_id) REFERENCES public.chats (id),
     FOREIGN KEY (sender_id) REFERENCES public.users (id)
 );
@@ -71,13 +75,68 @@ CREATE TABLE public.users_chats
 DROP TABLE IF EXISTS public.friends CASCADE;
 CREATE TABLE public.friends
 (
-    id SERIAL PRIMARY KEY,
-    requester_id INT NOT NULL,
-    addresser_id INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    id                 SERIAL PRIMARY KEY,
+    requester_id       INT,
+    addresser_id       INT,
+    status             VARCHAR(255),
     FOREIGN KEY (requester_id) REFERENCES public.users (id),
     FOREIGN KEY (addresser_id) REFERENCES public.users (id),
-    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_active BOOLEAN NOT NULL DEFAULT FALSE
+    creation_date      TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    is_active          BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS public.subscriptions CASCADE;
+CREATE TABLE public.subscriptions
+(
+    id            SERIAL PRIMARY KEY,
+    user_id       INT,
+    subscriber_id INT,
+    is_subscribed BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES public.users (id),
+    FOREIGN KEY (subscriber_id) REFERENCES public.users (id)
+);
+
+DROP TABLE IF EXISTS public.comments CASCADE;
+CREATE TABLE public.comments
+(
+    id                 SERIAL PRIMARY KEY,
+    commenter_id       INT     NOT NULL,
+    post_id            INT     NOT NULL,
+    text               VARCHAR(1000),
+    requester_id       INT,
+    addresser_id       INT,
+    status             VARCHAR(255),
+    FOREIGN KEY (requester_id) REFERENCES public.users (id),
+    FOREIGN KEY (addresser_id) REFERENCES public.users (id),
+    creation_date      TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    is_active          BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS public.posts CASCADE;
+CREATE TABLE public.posts
+(
+    id                 SERIAL PRIMARY KEY,
+    text_content       VARCHAR(1000),
+    attachments        TEXT[],
+    is_edited          BOOLEAN NOT NULL DEFAULT FALSE,
+    original_post_id   INT,
+    user_id            INT,
+    FOREIGN KEY (user_id) REFERENCES public.users (id),
+    creation_date      TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    is_active          BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS public.favorites CASCADE;
+CREATE TABLE public.favorites
+(
+    id                 SERIAL PRIMARY KEY,
+    selected_post_id   INT,
+    user_selector_id   INT,
+    FOREIGN KEY (user_selector_id) REFERENCES public.users (id),
+    creation_date      TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    is_active          BOOLEAN NOT NULL DEFAULT FALSE
 );
