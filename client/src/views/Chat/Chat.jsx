95318f3b0dbc6cc0ef6ea34594858ec7e4361434
Daniil Shcherbakov <djdaniil46@gmail.com>
2024-02-29 21:44:02 +0100

    base

--- client/src/views/Chat/Chat.jsx
+++ client/src/views/Chat/Chat.jsx
@@ -1,100 +1,129 @@
 import { useState, useEffect, useRef } from 'react';
 import './Chat.scss';
 import cx from 'classnames';
-import { API_URL, instance } from "../../api/config";
 import { useLocation } from 'react-router-dom';
 import { AiOutlineDelete } from "react-icons/ai";
 
 import { withLayout } from "../../hooks/withLayout"
+import { useDeleteMessageMutation, useGetMessagesQuery, useSendMessageMutation } from '../../store/services/chatService';
 
 
 
 const Messages = () => {
   const location = useLocation();
-  const  chatId  = location.state.chatId;
+  const chatId = location.state.chatId;
   console.log(chatId);
 
-  const [messages, setMessages] = useState([]);
+  const [messagesData, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
+  const [page, setPage] = useState(0)
   const contentRef = useRef();
 
+  const { data: messages, isLoading } = useGetMessagesQuery({ page, chatId })
+  const [sendMessage] = useSendMessageMutation()
+  const [deleteMessage] = useDeleteMessageMutation()
+
   useEffect(() => {
-    instance.get(`http://localhost:9000/api/messages?page=0&quantity=30&chatId=${chatId}`)
-      .then((response) => {
-        setMessages(response.data)
-        console.log(response);
-        scrollBottom();
-    })
-      
-      .catch((error) => console.error('Data error:', error));
-  }, []);
+    if (!isLoading && messages && messages.length > 0) {
+      setMessages(messages)
+    }
+  }, [isLoading, messages])
+
+  // useEffect(() => {
+  //   instance.get(`http://localhost:9000/api/messages?page=0&quantity=30&chatId=${chatId}`)
+  //     .then((response) => {
+  //       setMessages(response.data)
+  //       console.log(response);
+  //       scrollBottom();
+  //     })
+
+  //     .catch((error) => console.error('Data error:', error));
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  // const handleSendMessage = async (event) => {
+  //   if (event.key === 'Enter') {
+  //     if (newMessage.trim() !== '') {
+
+  //       try {
+  //         await instance.post(`http://localhost:9000/api/messages?chatId=${chatId}`, { text: newMessage });
+  //         const response = await instance.get(`http://localhost:9000/api/messages?page=0&quantity=30&chatId=${chatId}`)
+  //         console.log(response);
+  //         setMessages(response.data);
+  //         setNewMessage('');
+  //       } catch (error) {
+  //         console.error('Send message error:', error);
+  //       }
+
+  //     }
+  //   }
+  // };
 
   const handleSendMessage = async (event) => {
     if (event.key === 'Enter') {
       if (newMessage.trim() !== '') {
 
-      try {
-        await instance.post(`http://localhost:9000/api/messages?chatId=${chatId}`, {text: newMessage});
-        const response = await instance.get(`http://localhost:9000/api/messages?page=0&quantity=30&chatId=${chatId}`)
-        console.log(response);
-        setMessages(response.data);
-        setNewMessage('');
-      } catch (error) {
-        console.error('Send message error:', error);
-      }
-        
+        try {
+          const response = await sendMessage({ chatId, text: newMessage })
+          console.log(response.data);
+          setMessages([...messagesData, response.data]);
+          setNewMessage('');
+        } catch (error) {
+          console.error('Send message error:', error);
+        }
+
       }
     }
   };
 
-  const deleteMessage = async (item) => {
+  const handleDeleteMessage = async (item) => {
 
-      try {
-        console.log(item.id);
-        await instance.delete(`http://localhost:9000/api/messages/${item.id}`);
-        const response = await instance.get(`http://localhost:9000/api/messages?page=0&quantity=30&chatId=${chatId}`)
-        console.log(response);
-        setMessages(response.data);
-        setNewMessage('');
-      } catch (error) {
-        console.error('Delete message error:', error);
-      }     
+    try {
+      console.log(item.id);
+      await deleteMessage({ messageId: item.id })
+      // const response = await instance.get(`http://localhost:9000/api/messages?page=0&quantity=30&chatId=${chatId}`)
+      // console.log(response);
+      // setMessages(response.data);
+      setNewMessage('');
+    } catch (error) {
+      console.error('Delete message error:', error);
+    }
   };
 
-    
-    const scrollBottom = () => {
-      const contentHeight = contentRef.current.clientHeight;
-      console.log(contentRef);
-      console.log(contentHeight);
-      window.scrollTo({
-        bottom: contentHeight,
-        behavior: 'smooth',
-      });
-    }
-  
-    /*useEffect(scrollBottom, []);*/
-  
-    return (
+
+  const scrollBottom = () => {
+    const contentHeight = contentRef.current.clientHeight;
+    console.log(contentRef);
+    console.log(contentHeight);
+    window.scrollTo({
+      bottom: contentHeight,
+      behavior: 'smooth',
+    });
+  }
+
+  /*useEffect(scrollBottom, []);*/
+
+  return (
     <>
       <div className="chat-container" >
         <div className="chat-messages" ref={contentRef}>
-          {messages.map((message, index) => (
-           <div key={index} className={cx('message-item', {'user': message.senderId === 1}, {"bot":message.senderId !== 1})}>
-            <div className={cx(messages[index + 1] === undefined || {'message-avatar' : (messages[index + 1].senderId === 1)})}></div>
-             <div className="message-body">
-              <div className='message-text'>
-                {message.text}
+          {messagesData.map((message, index) => (
+            <div key={index} className={cx('message-item', { 'user': message.senderId === 1 }, { "bot": message.senderId !== 1 })}>
+              <div className={cx(messages[index + 1] === undefined || { 'message-avatar': (messages[index + 1].senderId === 1) })}></div>
+              <div className="message-body">
+                <div className='message-text'>
+                  {message.text}
+                </div>
+                <div className="message-img">
+
+                </div>
               </div>
-              <div className="message-img">
-                
+              <div className='message-options'>
+                <div className='message-options_option' onClick={() => handleDeleteMessage(message)}><AiOutlineDelete /></div>
+                <div className='message-options_option'></div>
+                <div className='message-options_option'></div>
               </div>
-             </div>
-            <div className='message-options'>
-              <div className='message-options_option' onClick={() => deleteMessage(message)}><AiOutlineDelete /></div>
-              <div className='message-options_option'></div>
-              <div className='message-options_option'></div>
             </div>
-           </div>
           ))}
         </div>
         <div className="chat-input">
@@ -108,7 +137,7 @@ const Messages = () => {
         </div>
       </div>
     </>
-    )
+  )
 }
 
 const Chat = withLayout(Messages)
