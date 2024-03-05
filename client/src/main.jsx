2e12521cd76520b1ad3a6eea5712009bad3f6ab5
Daniil Shcherbakov <djdaniil46@gmail.com>
2024-03-06 00:02:26 +0100

    fix eslint

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,21 @@
 //libs
-import React from "react";
-import {ThemeProvider} from '@mui/material'
+import { ThemeProvider } from '@mui/material'
 import ReactDOM from 'react-dom/client'
-import {theme} from './theme'
+import { theme } from './theme'
 //routes
-import {App} from './views/routes/Routes'
+import { App } from './views/routes/Routes'
 //styles
 import 'react-toastify/dist/ReactToastify.css';
 import './index.scss'
 //redux
-import {store} from './store'
-import {Provider} from 'react-redux'
+import { store } from './store'
+import { Provider } from 'react-redux'
 //configs
 import '../src/lib/firebase/firebaseConfig'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
+  // <React.StrictMode>
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       <App />
@@ -24,4 +23,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </Provider>
   // </React.StrictMode>,
 )
-;
+  ;
