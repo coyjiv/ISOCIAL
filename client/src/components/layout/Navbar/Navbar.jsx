5a18f17268ab85c516f02b14ca6ccb886235f16d
Daniil Shcherbakov <djdaniil46@gmail.com>
2024-03-06 00:16:17 +0100

    fix

--- client/src/components/layout/Navbar/Navbar.jsx
+++ client/src/components/layout/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { navbarLinks } from "../../../data/navbarLinks";
 import MessengerButton from "./actions/MessengerButton";
 import { HeaderLinks } from "./HeaderLinks";
