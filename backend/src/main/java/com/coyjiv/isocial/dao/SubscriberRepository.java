c5ae972ec032629ba19a4b84a7333e27d8b43b06
romanstebeletskyy1 <romanstebeletskyy1@gmail.com>
2024-03-06 21:04:34 +0200

    checkstyle fixed

--- backend/src/main/java/com/coyjiv/isocial/dao/SubscriberRepository.java
+++ backend/src/main/java/com/coyjiv/isocial/dao/SubscriberRepository.java
@@ -19,7 +19,8 @@ public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
   Long getCountSubscriptionsCountBySubscriberId(@Param("subscriberId")Long subscriberId);
 
   @Query("SELECT s FROM Subscriber s WHERE s.userId = :id AND s.subscriberId = :subscriberId  AND s.isActive = true")
-  Optional<Subscriber> findSubscriberByUserIdAndSubscriberId(@Param("id")Long userid,@Param("subscriberId") Long subscriberId);
+  Optional<Subscriber> findSubscriberByUserIdAndSubscriberId(@Param("id")Long userid,
+                                                             @Param("subscriberId") Long subscriberId);
 
   @Query("SELECT s FROM Subscriber s WHERE s.userId = :userId AND s.isActive = true")
   List<Subscriber> getSubscribers(@Param("userId")Long userId, Pageable pageable);
