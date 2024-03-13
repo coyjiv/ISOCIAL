a2dc5849f1eb6975122aa002813b5b05d8db699a
75hvu75 <75hvu75@gmail.com>
2024-03-13 11:42:46 +0200

    Fixed tabs in file FriendService.java

--- backend/src/main/java/com/coyjiv/isocial/service/friend/FriendService.java
+++ backend/src/main/java/com/coyjiv/isocial/service/friend/FriendService.java
@@ -162,9 +162,11 @@ public class FriendService implements IFriendService {
     }
 
     Optional<Friend> activeFriendship =
-friendRepository.findByRequesterAndAddresserAndStatusAndIsActive(user.get(),friendUser.get(), UserFriendStatus.FRIEND, true);
+			friendRepository.findByRequesterAndAddresserAndStatusAndIsActive(user.get(),friendUser.get(), 
+				UserFriendStatus.FRIEND, true);
     Optional<Friend> activeFriendship1 = 
-friendRepository.findByRequesterAndAddresserAndStatusAndIsActive(friendUser.get(),user.get(), UserFriendStatus.FRIEND,true);
+			friendRepository.findByRequesterAndAddresserAndStatusAndIsActive(friendUser.get(),user.get(), 
+				UserFriendStatus.FRIEND,true);
     if (activeFriendship.isPresent()) {
       Friend friend = activeFriendship.get();
       friend.setActive(false);
