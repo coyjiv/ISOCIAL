11bf044abb20fe756b5412d8e6b1c3deee5564f7
andriistebeletskyi <andriysteb.2005@gmail.com>
2024-03-20 19:54:17 +0200

    new friends endpoints US53-54-56-51

--- backend/src/main/resources/db/data-h2.sql
+++ backend/src/main/resources/db/data-h2.sql
@@ -1,67 +1,63 @@
-INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, gender,
-                          is_private, last_seen, date_of_birth, creation_date, last_modified_date, is_active,
-                          is_premium, premium_nickname, premium_emoji, activity_status)
-VALUES ('John', 'Doe', 'test@example.com', 'New York', '$2a$10$5OPwzIZ2N8CIeWUA2q9t0OzoI1kB7cvf1qDGwyxeiBFEAR3h2Psmu',
-        ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner.jpg',
-        'A short bio about John Doe', 'NOT_SPECIFIED    ', FALSE, CURRENT_TIMESTAMP,
-        '1990-01-15', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null, null, 'OFFLINE');
+INSERT INTO public.users ( first_name, last_name, email, city, password, avatars_url, banner_url, bio, birth_place, study_place,
+                          is_private, last_seen, activity_status, gender, date_of_birth, creation_date, last_modified_date,
+                          is_premium, premium_nickname, premium_emoji, is_active)
+VALUES ('John', 'Doe', 'test@example.com', 'New York',
+        '$2a$10$5OPwzIZ2N8CIeWUA2q9t0OzoI1kB7cvf1qDGwyxeiBFEAR3h2Psmu', ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner.jpg',
+        'A short bio about John Doe', 'Lviv', 'Cambridge', FALSE, CURRENT_TIMESTAMP, 'OFFLINE', 'NOT_SPECIFIED', '1990-02-1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, null, null, true);
 INSERT INTO public.roles (user_id, name)
 VALUES (1, 'ROLE_USER');
 
-INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, gender,
-                          is_private, last_seen, date_of_birth, creation_date, last_modified_date, is_active,
-                          is_premium, premium_nickname, premium_emoji, activity_status)
+INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, birth_place, study_place,
+                          is_private, last_seen, activity_status, gender, date_of_birth, creation_date, last_modified_date,
+                          is_premium, premium_nickname, premium_emoji, is_active)
 
 VALUES ('Alice', 'Johnson', 'test2@example.com', 'Los Angeles',
         '$2a$10$5OPwzIZ2N8CIeWUA2q9t0OzoI1kB7cvf1qDGwyxeiBFEAR3h2Psmu', ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner2.jpg',
-        'Bio for Alice Johnson', 'MALE', FALSE, CURRENT_TIMESTAMP,
-        '1985-03-20', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null, null, 'OFFLINE');
+        'Bio for Alice Johnson', 'Kyiv', 'Cambridge', FALSE, CURRENT_TIMESTAMP, 'OFFLINE', 'MALE', '1990-02-7', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, null, null, true);
 
 INSERT INTO public.roles (user_id, name)
 VALUES (2, 'ROLE_USER');
 
 
-INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, gender,
-                          is_private, last_seen, date_of_birth, creation_date, last_modified_date, is_active,
-                          is_premium, premium_nickname, premium_emoji, activity_status)
+INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, birth_place, study_place,
+                          is_private, last_seen, activity_status, gender, date_of_birth, creation_date, last_modified_date,
+                          is_premium, premium_nickname, premium_emoji, is_active)
 
-VALUES ('Bob', 'Smith', 'bob.smith@example.com', 'Chicago', 'mypassword', ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner3.jpg',
-        'Bob Smith bio', 'MALE', FALSE, CURRENT_TIMESTAMP,
-        '1992-07-08', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null, null, 'OFFLINE');
+VALUES ('Bob', 'Smith', 'bob.smith@example.com', 'Chicago',
+        'mypassword', ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner3.jpg',
+        'Bob Smith bio', 'Lviv', 'Harvard', FALSE, CURRENT_TIMESTAMP, 'OFFLINE', 'MALE', '1990-01-29', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, null, null, true);
 
 INSERT INTO public.roles (user_id, name)
 VALUES (3, 'ROLE_USER');
 
-INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, gender,
-                          is_private, last_seen, date_of_birth, creation_date, last_modified_date, is_active,
-                          is_premium, premium_nickname, premium_emoji, activity_status)
-VALUES ('Eva', 'Martinez', 'eva.martinez@example.com', 'San Francisco', 'securepass123',
-        ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner4.jpg',
-        'Evas bio description', 'MALE', FALSE, CURRENT_TIMESTAMP,
-        '1988-12-01', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null, null, 'OFFLINE');
+INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, birth_place, study_place,
+                          is_private, last_seen, activity_status, gender, date_of_birth, creation_date, last_modified_date,
+                          is_premium, premium_nickname, premium_emoji, is_active)
+VALUES ('Eva', 'Martinez', 'eva.martinez@example.com', 'San Francisco',
+        'securepass123', ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner4.jpg',
+        'Evas bio description', 'Lviv', Cambridge, FALSE, CURRENT_TIMESTAMP, 'OFFLINE', 'MALE', '1980-02-1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, null, null, true);
 
 INSERT INTO public.roles (user_id, name)
 VALUES (4, 'ROLE_USER');
 
-INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, gender,
-                          is_private, last_seen, date_of_birth, creation_date, last_modified_date, is_active,
-                          is_premium, premium_nickname, premium_emoji, activity_status)
+INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, birth_place, study_place,
+                          is_private, last_seen, activity_status, gender, date_of_birth, creation_date, last_modified_date,
+                          is_premium, premium_nickname, premium_emoji, is_active)
 
 VALUES ('Michael', 'Williams', 'michael.williams@example.com', 'Miami', 'password123', ARRAY ['https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'], 'banner5.jpg',
-        'Michaels bio', 'MALE', FALSE, CURRENT_TIMESTAMP,
-        '1975-06-25', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null, null, 'OFFLINE');
+        'Michaels bio', 'Miami', 'University of Miami', FALSE, CURRENT_TIMESTAMP, 'OFFLINE', 'MALE',
+        '1990-02-1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null, null);
 
 INSERT INTO public.roles (user_id, name)
 VALUES (5, 'ROLE_USER');
 
 
-INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, gender,
-                          is_private, last_seen, date_of_birth, creation_date, last_modified_date, is_active,
-                          is_premium, premium_nickname, premium_emoji, activity_status)
-VALUES ('Sophia', 'Lee', 'sophia.lee@example.com', 'Seattle', 'strongpassword456', ARRAY [], 'banner6.jpg',
-        'Sophias bio information', 'MALE', FALSE, CURRENT_TIMESTAMP,
-
-        '1995-09-10', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null , null , 'OFFLINE');
+INSERT INTO public.users (first_name, last_name, email, city, password, avatars_url, banner_url, bio, birth_place, study_place,
+                          is_private, last_seen, activity_status, gender, date_of_birth, creation_date, last_modified_date,
+                          is_premium, premium_nickname, premium_emoji, is_active)
+VALUES ('Sophia', 'Lee', 'sophia.lee@example.com', 'Seattle', 'strongpassword456', ARRAY[]::VARCHAR[], 'banner6.jpg',
+        'Sophias bio information', 'Seattle', 'University of Washington', FALSE, CURRENT_TIMESTAMP, 'OFFLINE', 'FEMALE',
+        '1995-09-10', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, false, null , null);
 
 INSERT INTO public.comments (commenter_id, post_id, text, creation_date, last_modified_date, is_active)
 VALUES (1, 1, '11111111', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true);
@@ -172,11 +168,13 @@ VALUES (1, 1, 'POST', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 INSERT INTO public.friends (requester_id, addresser_id, status, creation_date, last_modified_date, is_active)
 VALUES (4, 1, 'FRIEND', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
 INSERT INTO public.friends (requester_id, addresser_id, status, creation_date, last_modified_date, is_active)
-VALUES (3, 1, 'REQUEST_SENT', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
+VALUES (3, 1, 'FRIEND', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
 INSERT INTO public.friends (requester_id, addresser_id, status, creation_date, last_modified_date, is_active)
-VALUES (1, 5, 'REQUEST_SENT', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
+VALUES (1, 2, 'FRIEND', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
 INSERT INTO public.friends (requester_id, addresser_id, status, creation_date, last_modified_date, is_active)
-VALUES (6, 1, 'REQUEST_SENT', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
+VALUES (6, 1, 'FRIEND', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
+INSERT INTO public.friends (requester_id, addresser_id, status, creation_date, last_modified_date, is_active)
+VALUES (1, 5, 'FRIEND', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
 
 
 
