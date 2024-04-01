8237c7ffdff171dd833c93a3ea8b519bc0eb188f
Daniil Shcherbakov <djdaniil46@gmail.com>
2024-04-01 06:32:20 +0200

    cypress project id

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: '61ruke',
   e2e: {
     experimentalModifyObstructiveThirdPartyCode: true,
     setupNodeEvents(on, config) {
