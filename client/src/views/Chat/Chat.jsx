864e3fb03ba0672dbfc4fea312d552d8610a1235
Sviatoslav <svatoslavvolk@gmail.com>
2024-04-01 02:51:33 +0200

    Improvements for chat ( delete, route after delete chat )

--- client/src/views/Chat/Chat.jsx
+++ client/src/views/Chat/Chat.jsx
@@ -1,16 +1,12 @@
 import { useEffect } from "react";
 import "./Chat.scss";
-import {
-  useGetChatRecipientQuery,
-} from "../../store/services/chatService";
+import { useGetChatRecipientQuery } from "../../store/services/chatService";
 import { useParams, useNavigate } from "react-router";
 import PropTypes from "prop-types";
 import { withLayout } from "../../hooks/withLayout";
 import { useSelector, useDispatch } from "react-redux";
 import { setSelectedChat } from "../../store/chatSlice";
 import ChatView from "./ChatView";
-
-
 
 const ChatPage = () => {
   const navigate = useNavigate();
@@ -19,11 +15,17 @@ const ChatPage = () => {
   if (!paramsId) {
     console.log("No params id");
 
-    navigate('/chats')
+    navigate("/chats");
   }
 
   const selectedChat = useSelector((state) => state.chat.selectedChat);
-  const { data: chatData, isLoading, isSuccess } = useGetChatRecipientQuery(paramsId, { skip: !paramsId });
+  const {
+    data: chatData,
+    isLoading,
+    isSuccess,
+  } = useGetChatRecipientQuery(paramsId, { skip: !paramsId });
+
+  console.log(paramsId);
   console.log(chatData, "chat data");
 
   const dispatch = useDispatch();
@@ -31,26 +33,24 @@ const ChatPage = () => {
   useEffect(() => {
     if (!isLoading && !isSuccess) {
       console.log("Chat not found");
-      navigate('/chats')
+      navigate("/chats");
     }
-  }, [isLoading, isSuccess, navigate])
+  }, [isLoading, isSuccess, navigate]);
 
   useEffect(() => {
-    if (selectedChat === null && (chatData && chatData?.id)) {
+    if (selectedChat === null && chatData && chatData?.id) {
       dispatch(setSelectedChat(chatData));
     }
-  }, [chatData, dispatch, selectedChat])
+  }, [chatData, dispatch, selectedChat]);
 
   useEffect(() => {
     return () => {
       dispatch(setSelectedChat(null));
-    }
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
-  return (
-    <ChatView id={paramsId} />
-  );
+  return <ChatView id={paramsId} />;
 };
 
 ChatPage.propTypes = {
--- client/src/views/Chat/Chats.jsx
+++ client/src/views/Chat/Chats.jsx
@@ -1,16 +1,15 @@
-import { withLayout } from "../../hooks/withLayout"
+import { withLayout } from "../../hooks/withLayout";
 import { withWebsocket } from "../../hooks/withWebsocket.jsx";
 
 import { useState } from "react";
 
 import { ChatModal } from "./ChatModal";
-import Fab from '@mui/material/Fab';
-import EditIcon from '@mui/icons-material/Edit';
+import Fab from "@mui/material/Fab";
+import EditIcon from "@mui/icons-material/Edit";
 import "./Chat.scss";
 import ChatView from "./ChatView/index.jsx";
 
 const ChatContainer = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => {
@@ -18,8 +17,6 @@ const ChatContainer = () => {
   };
 
   const handleClose = () => setIsOpen(false);
-
-
 
   return (
     <div className="chats">
@@ -29,12 +26,17 @@ const ChatContainer = () => {
         modalText="Select Friend"
       />
       <ChatView />
-      <Fab onClick={handleOpen} sx={{ position: 'fixed', bottom: '20px', right: '20px' }} color="primary" aria-label="edit">
+      <Fab
+        onClick={handleOpen}
+        sx={{ position: "fixed", bottom: "20px", right: "20px" }}
+        color="primary"
+        aria-label="edit"
+      >
         <EditIcon />
       </Fab>
     </div>
   );
 };
 
-const Chats = withLayout(withWebsocket(ChatContainer))
-export default Chats
+const Chats = withLayout(withWebsocket(ChatContainer));
+export default Chats;
